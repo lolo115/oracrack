b5564c7cd2c11dacf833e875f2d14ef03b19d106
lolo115 <leturgezl@gmail.com>
2017-12-13 11:26:42 +0100

    Add files via upload

---
+++ ora10g_hash_bf.py
@@ -0,0 +1,61 @@
+import hashlib
+from Crypto.Cipher import DES
+from binascii import hexlify, unhexlify
+from passlib.crypto.des import des_encrypt_block
+import sys
+import os
+
+ORACLE10_MAGIC = b"\x01\x23\x45\x67\x89\xAB\xCD\xEF"
+
+def des_cbc_encrypt(key, value, iv=b'\x00' * 8, pad=b'\x00'):
+    value += pad * (-len(value) % 8) # null pad to multiple of 8
+    hash = iv # start things off
+    for offset in xrange(0,len(value),8):
+        chunk = xor_bytes(hash, value[offset:offset+8])
+        hash = des_encrypt_block(key, chunk)
+    return hash
+
+def calc_checksum(user, secret):
+    input = (user+secret).upper().encode("utf-16-be")
+    hash = des_cbc_encrypt(ORACLE10_MAGIC, input)
+    hash = des_cbc_encrypt(hash, input)
+    return hexlify(hash).decode("ascii").upper()
+
+def int_to_bytes(value, count):
+    return unhexlify(('%%0%dx' % (count<<1)) % value)
+
+def bytes_to_int(value):
+    return int(hexlify(value),16)
+
+def xor_bytes(l, r):
+    return int_to_bytes(bytes_to_int(l) ^ bytes_to_int(r), len(l))
+
+if len(sys.argv) != 4:
+	print ("ERROR")
+	sys.exit(1)
+
+print ("DICTFILE = "+sys.argv[1])
+print ("USERNAME = "+sys.argv[2])
+print ("HASH     = "+sys.argv[3])
+
+dictfile=sys.argv[1]
+username=sys.argv[2]
+submitted_hash=sys.argv[3].upper()
+
+if not os.path.isfile(dictfile):
+	print ("ERROR: Dictionary file: "+dictfile+" is not a regular file")
+	sys.exit(1)
+
+f=open(dictfile, "r")
+for line in f:
+	p2test=line.strip('\n')
+        if calc_checksum(username,p2test) == submitted_hash.upper():
+		print ("PASSWORD FOUND FOR USER "+username+" !!! PASSWORD IS: "+p2test)
+                print ("------")
+		print ("Be careful, the found password is case unsensitive. The real password can include upper character(s)")
+		print ("Now generate all combinations for this password and run ora11g_hash_bf.py script to find the case sensitive password")
+		sys.exit(0)
+
+print ("Password not found")
+
+
---
+++ ora11g_hash_bf.py
@@ -0,0 +1,37 @@
+import hashlib
+import binascii as ba
+import sys
+import os
+
+if len(sys.argv) != 4:
+        print ("ERROR")
+        sys.exit(1)
+
+print ("DICTFILE = "+sys.argv[1])
+print ("USERNAME = "+sys.argv[2])
+print ("HASH     = "+sys.argv[3])
+
+dictfile=sys.argv[1]
+username=sys.argv[2]
+submitted_hash=sys.argv[3]
+hash_digest=submitted_hash[0:40].upper()
+hash_salt=submitted_hash[40:60].upper()
+
+
+if not os.path.isfile(dictfile):
+        print ("ERROR: Dictionary file: "+dictfile+" is not a regular file")
+        sys.exit(1)
+
+f=open(dictfile, "r")
+for line in f:
+        p2test=line.strip('\n')
+	source1_to_hash=p2test+ba.unhexlify(hash_salt)        
+        sha1_hash1=hashlib.sha1(source1_to_hash).hexdigest()
+#	print ("USERNAME = "+username+" - PASSWORD = "+p2test+" - S:"+sha1_hash1.upper()+hash_salt)
+
+        if sha1_hash1.upper()+hash_salt == submitted_hash.upper():
+                print ("PASSWORD FOUND FOR USER "+username+" !!! PASSWORD IS: "+p2test)
+		sys.exit(0)
+print("Password not found")
+
+
